7adb3e1519714a2fb3410a4ef5c42b85a65c0a2a
Julia Shreckhise <julia.shreckhise@gmail.com>
2018-04-21 18:23:19 -0400

    manager functionality except new product

--- bamazonManager.js
+++ bamazonManager.js
@@ -14,60 +14,148 @@ connection.connect(function (err) {
     displayOptions();
 });
 
+
+//for validations
+function isNum(num) {
+    return (isNaN(parseInt(num)) ? "Please type in an integer" : true);
+}
+
 function displayOptions() {
     inquirer.prompt([
         {
             name: "options",
             type: "list",
-            message: "What would you like to do?",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
-            //add filter?
+            message: "\nWhat would you like to do?",
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", new inquirer.Separator(), "Exit"]
         }
     ])
         .then(function (answer) {
-            switch (answer){
-            // If a manager selects View Products for Sale
-            case "View products for Sale":
-            listItems();
-            // If a manager selects View Low Inventory
-            case "View Low Inventory":
-            lowInventory();
-            // If a manager selects Add to Inventory 
-            case "Add to Inventory":
-            addInventory();
-            // If a manager selects Add New Product
-            case "Add New Product":
-            newProduct();
+            switch (answer.options) {
+
+                case "View Products for Sale":
+                    listItems();
+                    break;
+
+                case "View Low Inventory":
+                    lowInventory();
+                    break;
+
+                case "Add to Inventory":
+                    addInventory();
+                    break;
+
+                case "Add New Product":
+                    newProduct();
+                    break
+
+                case "Exit":
+                    connection.end();
             }
         });
 }
 
-
-
+//lists all items for sale
 function listItems() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
         res.forEach(function (item) {
-            console.log(`${item.id} ${item.product_name}, $${item.price}, ${item.stock_quantity} in stock`);
+            console.log(`
+            ${item.id} ${item.product_name}, $${item.price}, ${item.stock_quantity} in stock`);
         });
+        next();
     });
 }
 
-function lowInventory(){
-    console.log("low");
-//then it should list all items with an inventory count lower than five.
+//list all items with inventory count lower than 20
+function lowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity < 20", function (err, res) {
+        if (err) throw err;
+        console.log(`
+        LOW INVENTORY:`);
+        res.forEach(function (item) {
+            console.log(`
+            ${item.product_name}, ID: ${item.id}, Quantity in stock: ${item.stock_quantity} `);
+        });
+        next();
+    });
 }
 
-function addInventory(){
-    console.log("add");
-//prompt that will let the manager "add more" of any item currently in the store.
-    //prompt for item id and then quantity to add, select by id and add to stock_quantity
+//allow user to add quantity to existing items for sale
+function addInventory() {
+
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "\nWhat is the id of the product you wish to add inventory to?",
+            validate: isNum
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "\nHow many units would you like to add to inventory?",
+            validate: isNum
+        }
+    ])
+        .then(function (answers) {
+
+            connection.query("SELECT * FROM products WHERE ?", { id: answers.id }, function (err, res) {
+
+                var newStock = res[0].stock_quantity + parseInt(answers.quantity);
+                console.log(typeof answers.quantity);
+                var name = res[0].product_name;
+                var id = res[0].id;
+
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: newStock
+                        },
+                        {
+                            id: answers.id
+                        }
+                    ],
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log (`Successfully added inventory to item id ${id}, ${name}. Quantity now in stock: ${newStock}`);
+                        next();
+                    });
+            });
+        });
 }
 
+//allow user to add new product
 function newProduct() {
-    console.log("new");
+
     //allow the manager to add a completely new product to the store
-        //prompt for name of product, department, price, and quantity -> then add new row to table
-        //validate item (name) doesn't already exist?
+    //prompt for name of product, department, price, and quantity -> then add new row to table
+    //validate item (name) doesn't already exist?
+
+
+    next();
+}
+
+
+//ask if user want to do something else or want to exit
+function next() {
+    inquirer.prompt([
+        {
+            name: "next",
+            type: "list",
+            message: "\nWould you like to do something else?",
+            choices: ["Yes", "Exit"]
+        }
+    ])
+        .then(function (answer) {
+            switch (answer.next) {
+
+                case "Yes":
+                    displayOptions();
+                    break;
+
+                case "Exit":
+                    connection.end();
+            }
+        });
 }
